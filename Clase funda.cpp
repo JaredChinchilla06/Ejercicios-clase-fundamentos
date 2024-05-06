ca7741407c708629562ebf711402384f4d0625aa
JaredChinchilla06 <164411479+JaredChinchilla06@users.noreply.github.com>
2024-05-06 12:37:04 -0600

    Ejercicios clase funda

---
+++ Clase funda.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1, n = 12, r = 0;
+    while (i<=12)
+    {
+        r= n * i;
+        cout << r << "\n";
+        i++;
+    }
+    
+    return 0;
+}
